96d16926ff7c01aa4555449db136d21d1cd9bad0
bry <greatmaharg@gmail.com>
2025-05-11 23:04:10 -0400

    bleh

--- ocaml_pl0/bin/main.ml
+++ ocaml_pl0/bin/main.ml
@@ -1,1 +1,7 @@
-let () = print_endline "Hello, World!"
+let handle_file file = 
+  print_endline file;;
+
+let handle_all files = 
+  (Array.map(handle_file) files);;
+
+let () = (ignore @@ handle_all Sys.argv)
