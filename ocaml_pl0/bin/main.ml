d34724186c1d055445eea32ed849849f5757ea51
bry <greatmaharg@gmail.com>
2025-05-12 00:45:01 -0400

    skeleton of tokenizer

--- ocaml_pl0/bin/main.ml
+++ ocaml_pl0/bin/main.ml
@@ -1,7 +1,24 @@
+type token = 
+  | Period
+  | Write
+  | Literal of int
+  ;;
+
+let string_of_token: token -> string = function
+  | Period -> "."
+  | Write -> "WRITE"
+  | Literal i -> string_of_int i
+
+let tokenize: string -> token list = function
+  | _ -> []
+
+let handle_code (code : string) = 
+  print_endline @@ string_of_token Period; print_endline code
+
 let handle_path (path : string) = 
   let f = open_in path in
     let text = In_channel.input_all(f) in
-      print_endline text
+      handle_code text  
 
 let rec handle_all: string list -> unit = function
   | path :: rest -> (handle_path path); (handle_all rest)
