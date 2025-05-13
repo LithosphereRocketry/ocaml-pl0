13a668a1c16039712d14439dda3cdb6be9357165
Randymations <randyn@eblay.com>
2025-05-13 05:35:14 -0400

    add myMap

--- activities.ml
+++ activities.ml
@@ -11,9 +11,18 @@ let counting : int -> string = function
   | _ -> "I don't know"
 
 let add3 (n : int) : int = n + 3
-let add4 (n : int) : int = n + 2
+let add4 (n : int) : int = n + 4
 let add7 (n : int) : int = add4 @@ add3 n
 
+
+let rec myMap (func : int -> int) (l : int list) : int list =
+  match l with
+  | (current::rest) -> func current :: myMap func rest
+  | [] -> []
+
+let rec myMap (func : int -> int) : int list -> int list = function
+  | (current::rest) -> func current :: myMap func rest
+  | [] -> []
 
 let rec controlMap (func : string -> string) (strings : string list) : string list =
   match strings with
