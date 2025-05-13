58ff50c0d1bb5f9dd03935025bbf0d63815d8683
bry <greatmaharg@gmail.com>
2025-05-13 03:05:33 -0400

    Updated example with test cases

--- example.ml
+++ example.ml
@@ -17,7 +17,7 @@ let lex_token : string -> token = function
   | s -> invalid_arg s 
            
 let eval_binary (func : float -> float -> float) : float list -> float list = function
-  | a :: b :: tail -> func a b :: tail
+  | a :: b :: tail -> func b a :: tail
   | _ -> failwith "Not enough elements on stack!" 
 
 let eval_rpn_helper (stack : float list) : token -> float list = function
@@ -33,3 +33,64 @@ let eval_rpn (input : token list) : float =
  
 let eval (s : string) : float =
   eval_rpn @@ List.map lex_token @@ tokenize s 
+
+(* ==================== TEST CODE STARTS HERE ==================== *)
+(* Do not modify code below this point *)
+
+type test_result =
+  | Result of float
+  | InvalidArg
+  | Fail
+
+let tests : (string * test_result) list = [
+  "1 2 +", Result 3.;
+  "5 3 -", Result 2.;
+  "3 5 -", Result (-2.);
+  "3 2 *", Result 6.;
+  "6 2 /", Result 3.;
+  "3 6 /", Result 0.5;
+  "1 2 + 3 *", Result 9.;
+  "3 2 1 + *", Result 9.;
+  "-4 5 +", Result 1.;
+  "5 1 2 + 4 * 3 - +", Result 14.;
+  "4 2 5 * + 1 3 2 * + /", Result 2.;
+  "", InvalidArg;
+  "1 a +", InvalidArg;
+  "7 0 !", InvalidArg;
+  "+", Fail;
+  "1 + 2", Fail;
+  "1 +", Fail;
+  "1 2 + *", Fail;
+  "8 7 6 5 4 3 2 1 + + +", Result 10.;
+]
+
+let run_test : string * float -> bool = function
+  | (expr, expected) -> eval expr == expected
+
+let results_equal : test_result * test_result -> bool = function
+  | Result a, Result b -> a == b
+  | InvalidArg, InvalidArg -> true
+  | Fail, Fail -> true
+  | _ -> false
+
+let string_of_test_result : test_result -> string = function
+  | Result f -> string_of_float f
+  | InvalidArg -> "invalid_arg ..."
+  | Fail -> "failwith ..."
+
+let run_test : string * test_result -> int = function
+  | (expr, expected) -> let result = (try Result (eval expr)
+      with | Invalid_argument _ -> InvalidArg
+           | Failure _ -> Fail
+      ) in
+    if results_equal (result, expected) then 1
+    else (Printf.printf "Test %s failed: got %s, expected %s"
+      expr
+      (string_of_test_result result)
+      (string_of_test_result expected)
+    ; 0)
+
+let () = 
+  Printf.printf "%i of %i tests passed"
+  (List.fold_left (+) 0 @@ List.map run_test tests)
+  (List.length tests)
