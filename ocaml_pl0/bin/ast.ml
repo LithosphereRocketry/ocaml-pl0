01fae0b93d8927807a0e0dfad5925540a5e6e616
Randymations <randyn@eblay.com>
2025-05-23 06:02:57 -0400

    Print expr

--- ocaml_pl0/bin/ast.ml
+++ ocaml_pl0/bin/ast.ml
@@ -49,9 +49,18 @@ type procedure = {
 let string_of_constdef ((a : string), (b : int)) : string =
     a ^ " = " ^ string_of_int b
   
-let string_of_expr: expression -> string = function
+let rec string_of_expr: expression -> string = function
+  | Prefix (Plus, exp) -> getPre "+" exp
+  | Prefix (Minus, exp) -> getPre "-" exp
+  | Infix (exp1, Plus, exp2) -> getIn "+" exp1 exp2
+  | Infix (exp1, Minus, exp2) -> getIn "-" exp1 exp2
+  | Infix (exp1, Times, exp2) -> getIn "*" exp1 exp2
+  | Infix (exp1, Divide, exp2) -> getIn "/" exp1 exp2
   | Literal i -> string_of_int i
   | _ -> "unknown expression"
+
+and getPre (op : string) (exp : expression) : string = "(" ^ op ^ string_of_expr exp ^ ")"
+and getIn (op : string) (exp1 : expression) (exp2 : expression) : string = "(" ^ string_of_expr exp1 ^ op ^ string_of_expr exp2 ^ ")"
 
 let string_of_stmt: statement -> string = function
   | Display e -> "! " ^ string_of_expr e
@@ -62,7 +71,8 @@ let rec string_of_block (b : block) : string =
   "const " ^ String.concat ", " (List.map string_of_constdef b.constdef) ^ ";\n" ^
   "var " ^ String.concat ", " b.vardef ^ ";\n" ^
   String.concat "\n" (List.map string_of_proc b.procdef) ^ "\n" ^
-  string_of_stmt b.stmt ^ "\n."
+  string_of_stmt b.stmt ^ "\n"
 
-and string_of_proc (_ : procedure) : string = 
-  "(PROCEDURE)"
+and string_of_proc (p : procedure) : string = 
+  "procedure " ^ p.name ^ ";\n" ^
+  string_of_block p.body
