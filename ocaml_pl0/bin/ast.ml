910152c6593f414c6a7ad3c5494bd94770f3de18
Randymations <randyn@eblay.com>
2025-05-23 06:55:16 -0400

    Add ast var and add test cases

--- ocaml_pl0/bin/ast.ml
+++ ocaml_pl0/bin/ast.ml
@@ -49,7 +49,7 @@ type procedure = {
 let string_of_constdef ((a : string), (b : int)) : string =
     a ^ " = " ^ string_of_int b
   
-let rec string_of_expr: expression -> string = function
+let rec string_of_exp: expression -> string = function
   | Prefix (Plus, exp) -> getPre "+" exp
   | Prefix (Minus, exp) -> getPre "-" exp
   | Infix (exp1, Plus, exp2) -> getIn "+" exp1 exp2
@@ -57,15 +57,31 @@ let rec string_of_expr: expression -> string = function
   | Infix (exp1, Times, exp2) -> getIn "*" exp1 exp2
   | Infix (exp1, Divide, exp2) -> getIn "/" exp1 exp2
   | Literal i -> string_of_int i
-  | _ -> "unknown expression"
+  | Variable ident -> ident
 
-and getPre (op : string) (exp : expression) : string = "(" ^ op ^ string_of_expr exp ^ ")"
-and getIn (op : string) (exp1 : expression) (exp2 : expression) : string = "(" ^ string_of_expr exp1 ^ op ^ string_of_expr exp2 ^ ")"
+and getPre (op : string) (exp : expression) : string = "(" ^ op ^ string_of_exp exp ^ ")"
+and getIn (op : string) (exp1 : expression) (exp2 : expression) : string = "(" ^ string_of_exp exp1 ^ op ^ string_of_exp exp2 ^ ")"
 
-let string_of_stmt: statement -> string = function
-  | Display e -> "! " ^ string_of_expr e
+let getComp (op : string) (exp1 : expression) (exp2 : expression) : string = string_of_exp exp1 ^ " " ^ op ^ " " ^ string_of_exp exp2
+
+let string_of_cond: condition -> string = function
+  | Odd exp -> "odd " ^ string_of_exp exp
+  | Comparison (exp1, Equals, exp2) -> getComp "=" exp1 exp2
+  | Comparison (exp1, NotEquals, exp2) -> getComp "#" exp1 exp2
+  | Comparison (exp1, LessThan, exp2) -> getComp "<" exp1 exp2
+  | Comparison (exp1, LessEqual, exp2) -> getComp "<=" exp1 exp2
+  | Comparison (exp1, GreaterThan, exp2) -> getComp ">" exp1 exp2
+  | Comparison (exp1, GreaterEqual, exp2) -> getComp "=>" exp1 exp2
+
+let rec string_of_stmt: statement -> string = function
+  | Assignment (ident, exp) -> ident ^ ":=" ^ string_of_exp exp ^ "\n"
+  | Call ident -> "call " ^ ident ^ "\n"
+  | Query str -> "? " ^ str ^ "\n"
+  | Display e -> "! " ^ string_of_exp e ^ "\n"
+  | Begin stmts -> "begin\n" ^ String.concat ";\n" (List.map string_of_stmt stmts) ^ "end\n"
+  | If (cond, stmt) -> "if " ^ string_of_cond cond ^ " then " ^ string_of_stmt stmt
+  | While (cond, stmt) -> "while " ^ string_of_cond cond ^ " then " ^ string_of_stmt stmt
   | Empty -> ""
-  | _ -> "unknown statement"
 
 let rec string_of_block (b : block) : string = 
   "const " ^ String.concat ", " (List.map string_of_constdef b.constdef) ^ ";\n" ^
